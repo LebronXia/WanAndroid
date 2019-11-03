bc1d0c975dcb31db2d311e18d6d20a15aacbeb9b
zhengxiaobo <zhengxiaobo@jianbing.com>
2019-11-04 00:18:37 +0800

    update mvvm

---
+++ app/src/main/java/com/xiamu/wanandroid/mvvm/view/activity/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.xiamu.wanandroid.mvvm.view.activity
+
+import com.xiamu.baselibs.base.BaseActivity as BaseActivity
+
+/**
+ * Created by zhengxiaobo in 2019-11-03
+ */
+class LoginActivity: BaseActivity(){
+
+
+}
